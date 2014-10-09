4973ea11a0c65189533cdb5197bcd793ac38b2a7
Mark Essel <messel@gmail.com>
2014-10-09 18:15:23 -0400

    first commit, tryign to get SDAVAssetExportSession working with swift now

---
+++ SwiftVideo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftVideo
+//
+//  Created by messel on 10/9/14.
+//  Copyright (c) 2014 messel. All rights reserved.
+//
+
+import UIKit
+import GPUImage
+import SDAVAssetExportSession
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
